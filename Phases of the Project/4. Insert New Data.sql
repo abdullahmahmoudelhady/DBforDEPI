353962efa47b3c790f4c1e1de4ea459fcce7c410
abdullahmahmoudelhady <abdullahmahmoudelhady@gmail.com>
2024-07-29 19:40:09 +0300

    Create 4. Insert New Data.sql

---
+++ Phases of the Project/4. Insert New Data.sql
@@ -0,0 +1,7 @@
+INSERT INTO Employees VALUES
+(15340, 'Sara', 'Nabil', 'FHO', '2022-11-10', 'Finance Analyst', 22000.00),
+(16550, 'Omar', 'El-Sayed', 'ITHO', '2021-04-15', 'Mechanical Engineer', 32000.00);
+
+INSERT INTO Departments VALUES
+('SAP', 'ERP', 'HO'),
+('R&D', 'Research and Development', 'HO');
