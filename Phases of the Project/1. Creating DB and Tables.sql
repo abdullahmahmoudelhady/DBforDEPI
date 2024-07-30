1a6bb46cc738fcccec1a12eec8b2212c3edfdb49
abdullahmahmoudelhady <abdullahmahmoudelhady@gmail.com>
2024-07-30 22:17:27 +0300

    1. Creating DB and Tables.sql

--- Phases of the Project/1. Creating DB and Tables.sql
+++ Phases of the Project/1. Creating DB and Tables.sql
@@ -1,14 +1,13 @@
-CREATE DATABASE group1;
+CREATE DATABASE DBforDEPI;
 
 -- Employees Table
-USE group1;
+USE DBforDEPI;
 CREATE TABLE Employees (
 EmployeeID INT PRIMARY KEY, FirstName VARCHAR(20), LastName VARCHAR(20), DepartmentID VARCHAR(20), HireDate DATE, Position VARCHAR(50), Salary DECIMAL(10, 2)
 )
 
 -- Departments Table
-USE group1;
+USE DBforDEPI;
 CREATE TABLE Departments (
 DepartmentID VARCHAR(20) PRIMARY KEY, DepartmentName VARCHAR(50), Location VARCHAR(50)
 )
-
